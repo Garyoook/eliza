24b38edfa7d65cec184163abbdfe619c58a63c0e
yg9418 <yg9418@ic.ac.uk>
2019-06-04 20:03:01 +0100

    finished patr I and II, flaw exists in part II

---
+++ cmake-build-debug/CMakeFiles/eliza_skeleton.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/yg9418/C/past_test/eliza_skeleton/eliza.c" "/home/yg9418/C/past_test/eliza_skeleton/cmake-build-debug/CMakeFiles/eliza_skeleton.dir/eliza.c.o"
+  "/home/yg9418/C/past_test/eliza_skeleton/eliza_state.c" "/home/yg9418/C/past_test/eliza_skeleton/cmake-build-debug/CMakeFiles/eliza_skeleton.dir/eliza_state.c.o"
+  "/home/yg9418/C/past_test/eliza_skeleton/list.c" "/home/yg9418/C/past_test/eliza_skeleton/cmake-build-debug/CMakeFiles/eliza_skeleton.dir/list.c.o"
+  "/home/yg9418/C/past_test/eliza_skeleton/map.c" "/home/yg9418/C/past_test/eliza_skeleton/cmake-build-debug/CMakeFiles/eliza_skeleton.dir/map.c.o"
+  "/home/yg9418/C/past_test/eliza_skeleton/parser.c" "/home/yg9418/C/past_test/eliza_skeleton/cmake-build-debug/CMakeFiles/eliza_skeleton.dir/parser.c.o"
+  "/home/yg9418/C/past_test/eliza_skeleton/rule.c" "/home/yg9418/C/past_test/eliza_skeleton/cmake-build-debug/CMakeFiles/eliza_skeleton.dir/rule.c.o"
+  "/home/yg9418/C/past_test/eliza_skeleton/string_utils.c" "/home/yg9418/C/past_test/eliza_skeleton/cmake-build-debug/CMakeFiles/eliza_skeleton.dir/string_utils.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
